06ba3fb9bc4f467a482fd6890e3daeea41cde0ed
Peter Lee <peter.lee@berkeley.edu>
2018-01-09 00:45:10 -0600

    Added color data flow

--- src/RubiksCube/RubiksCube.js
+++ src/RubiksCube/RubiksCube.js
@@ -7,9 +7,54 @@ class RubiksCube extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            selected: 'Front'
+            selected: 'Front',
+            frontColorData: [],
+            leftColorData: [],
+            rightColorData: [],
+            topColorData: [],
+            bottomColorData: [],
+            backColorData: []
         };
         this.sides = ['Front', 'Left', 'Right', 'Top', 'Bottom', 'Back'];
+
+        this.updateColors = this.updateColors.bind(this);
+        this.updateSelectedFace = this.updateSelectedFace.bind(this);
+    }
+
+    updateColors(faceColors) {
+        const side = this.state.selected;
+        const sideColorData = side.toLowerCase() + 'ColorData';
+        const colorData = new Object();
+        colorData[sideColorData] = faceColors;
+        this.setState(colorData);
+        this.updateSelectedFace();
+
+        let readyToSolve = true;
+        let cubeColors = [];
+        this.sides.forEach((side) => {
+            let sideColors = this.state[side.toLowerCase() + 'ColorData'];
+            if (side === this.state.selected) {
+                sideColors = faceColors;
+            }
+            if (sideColors.length === 0) {
+                readyToSolve = false;
+            } else {
+                cubeColors.push(sideColors);
+            }
+        });
+        if (readyToSolve) {
+            this.props.loadSolver(cubeColors);
+        }
+    }
+
+    updateSelectedFace() {
+        let side = this.state.selected;
+        let idx = 0;
+        while (this.state.selected !== this.sides[idx]) idx++;
+        idx = (idx + 1) % this.sides.length;
+        this.setState({
+            selected: this.sides[idx]
+        });
     }
 
     componentDidMount() {
@@ -32,6 +77,7 @@ class RubiksCube extends Component {
                     key={side}
                     type={side}
                     selected={(this.state.selected === side) ? "true" : ""}
+                    colorData={this.state[side.toLowerCase() + 'ColorData']}
                     ref={side}></RubiksSide>
             );
         });
--- src/RubiksCube/RubiksSide.js
+++ src/RubiksCube/RubiksSide.js
@@ -9,28 +9,24 @@ class RubiksSide extends Component {
     }
 
     render() {
-        let boxes = [];
-        for (let i = 0; i < 9; i++) {
-            // Push divs here
-            // Then put divs in render return value and style        
-        }
+        let colorData = this.props.colorData
         return <div className={ "Rubiks-side " + 
                         (this.props.selected === 'true' ? "Rubiks-side-selected" : "") }>
             <h1 className="Rubiks-side-type">{this.props.type}</h1>
             <div className="Rubiks-side-row">
-                <RubiksSlot color="black"></RubiksSlot>
-                <RubiksSlot color="black"></RubiksSlot>
-                <RubiksSlot color="black"></RubiksSlot>
+                <RubiksSlot color={(colorData.length > 0 ? colorData[0][0] : "black")}></RubiksSlot>
+                <RubiksSlot color={(colorData.length > 0 ? colorData[0][1] : "black")}></RubiksSlot>
+                <RubiksSlot color={(colorData.length > 0 ? colorData[0][2] : "black")}></RubiksSlot>
             </div>
             <div className="Rubiks-side-row">
-                <RubiksSlot color="black"></RubiksSlot>
-                <RubiksSlot color="black"></RubiksSlot>
-                <RubiksSlot color="black"></RubiksSlot>
+                <RubiksSlot color={(colorData.length > 0 ? colorData[1][0] : "black")}></RubiksSlot>
+                <RubiksSlot color={(colorData.length > 0 ? colorData[1][1] : "black")}></RubiksSlot>
+                <RubiksSlot color={(colorData.length > 0 ? colorData[1][2] : "black")}></RubiksSlot>
             </div>
             <div className="Rubiks-side-row">
-                <RubiksSlot color="black"></RubiksSlot>
-                <RubiksSlot color="black"></RubiksSlot>
-                <RubiksSlot color="black"></RubiksSlot>
+                <RubiksSlot color={(colorData.length > 0 ? colorData[2][0] : "black")}></RubiksSlot>
+                <RubiksSlot color={(colorData.length > 0 ? colorData[2][1] : "black")}></RubiksSlot>
+                <RubiksSlot color={(colorData.length > 0 ? colorData[2][2] : "black")}></RubiksSlot>
             </div>
         </div>;
     }
